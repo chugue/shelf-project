2b6cb036243ba20f83beedb64d39cda633f105db
AI_Nomads <chugue85@gmail.com>
2024-07-07 16:06:45 +0900

    start shelf

---
+++ src/main/java/com/project/shelf/ShelfApplication.java
@@ -0,0 +1,13 @@
+package com.project.shelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelfApplication.class, args);
+    }
+
+}
