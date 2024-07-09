eed788de83ea3dfd8731892911efd17b49aa0d4b
LifeIsOne <didtmdgh258@gmail.com>
2024-07-09 13:50:59 +0900

    FEAT : @Repository 추가

--- src/main/java/com/project/shelf/book_history/BookHistoryRepository.java
+++ src/main/java/com/project/shelf/book_history/BookHistoryRepository.java
@@ -2,6 +2,8 @@ package com.project.shelf.book_history;
 
 import com.project.shelf.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookHistoryRepository extends JpaRepository<BookHistory, Integer> {
 }
--- src/main/java/com/project/shelf/sub_payment/SubPaymentRepository.java
+++ src/main/java/com/project/shelf/sub_payment/SubPaymentRepository.java
@@ -2,6 +2,8 @@ package com.project.shelf.sub_payment;
 
 import com.project.shelf.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SubPaymentRepository extends JpaRepository<SubPaymentRepository, Integer> {
+@Repository
+public interface SubPaymentRepository extends JpaRepository<SubPayment, Integer> {
 }
