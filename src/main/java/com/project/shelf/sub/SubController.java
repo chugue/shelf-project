aa035444ddf66e0b6d02f393848d7146c28dd213
kjh5848 <kjh5848@gmail.com>
2024-07-10 16:36:29 +0900

    [INIT] 셋팅 완료v2

---
+++ src/main/java/com/project/shelf/sub/SubController.java
@@ -0,0 +1,9 @@
+package com.project.shelf.sub;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class SubController {
+}
