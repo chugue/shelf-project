6f59a93af7aee06ca9e1f8961fae036ad1d24d9b
yeeun0118s <yeeun0118s@naver.com>
2024-07-10 14:36:44 +0900

    FEAT:베스트 셀러 가져오기 완료

--- src/main/java/com/project/shelf/book/BookRepository.java
+++ src/main/java/com/project/shelf/book/BookRepository.java
@@ -1,9 +1,18 @@
 package com.project.shelf.book;
 
 import com.project.shelf.author.Author;
+import com.project.shelf.book_history.BookHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    //베스트셀러 구하는 쿼리
+    @Query("SELECT b FROM BookHistory bh JOIN bh.book b GROUP BY b.id ORDER BY COUNT(bh.id) DESC")
+    List<Book> findBooksByHistory();
+
 }
