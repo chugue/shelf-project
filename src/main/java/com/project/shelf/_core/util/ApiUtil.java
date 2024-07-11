ba372f58fad7fcc33f5539be3e25c2941af00692
AI_Nomads <chugue85@gmail.com>
2024-07-12 01:21:48 +0900

    FIX: apiUtil

--- src/main/java/com/project/shelf/_core/util/ApiUtil.java
+++ src/main/java/com/project/shelf/_core/util/ApiUtil.java
@@ -7,17 +7,17 @@ public class ApiUtil<T> {
 
     private Integer status;
     private String msg;
-    private T body;
+    private T data;
 
     public ApiUtil(T body) {
         this.status = 200;
         this.msg = "성공";
-        this.body = body;
+        this.data = body;
     }
 
     public ApiUtil(Integer status, String msg) {
         this.status = status;
         this.msg = msg;
-        this.body = null;
+        this.data = null;
     }
 }
