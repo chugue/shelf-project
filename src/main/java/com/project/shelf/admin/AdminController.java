99163cb293627c58578dc1fc2e9d84b2f6e667c9
yeeun <yeeun@naver.com>
2024-07-16 04:23:04 +0900

    FEAT:책 상세보기 페이지 epub 빼고 완료

--- src/main/java/com/project/shelf/admin/AdminController.java
+++ src/main/java/com/project/shelf/admin/AdminController.java
@@ -1,15 +1,21 @@
 package com.project.shelf.admin;
 
+import com.project.shelf.book.Book;
+import com.project.shelf.book.BookService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class AdminController {
+
+    private final AdminService adminService;
+    private final BookService bookService;
 
     @GetMapping("/")
     public String mainPage(HttpServletRequest request) {
@@ -41,8 +47,11 @@ public class AdminController {
         return "admin/add-book";
     }
 
-    @GetMapping("/admin/book-detail")
-    public String getBookDetail(HttpServletRequest request) {
+    @GetMapping("/admin/book/{bookId}")
+    public String getBookDetail(HttpServletRequest request, @PathVariable Integer bookId) {
+        Book book = adminService.bookDetail(bookId);
+
+        request.setAttribute("book", book);
         return "admin/book-detail";
     }
 
