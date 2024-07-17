c0e033609359aeb6f19f83f9a057f4b853cd0668
AI_Nomads <chugue85@gmail.com>
2024-07-17 20:21:03 +0900

    FIX: path

--- src/main/resources/db/author_data.sql
+++ src/main/resources/db/author_data.sql
@@ -1,6 +1,6 @@
 --3. 저자 --
 INSERT INTO author_tb(name, author_intro, created_at, updated_at)
-values ('저자1','소개1', NOW(), NOW()),
+values ('찰스 두히그','퓰리처상 수상 저널리스트이자 미국 최고의 논픽션 작가', NOW(), NOW()),
        ('tvN제작진','<벌거벗은 한국사> 제작진 팀', NOW(), NOW()),
        ('알렉스 안도릴','1700만 부를 팔은 사람', NOW(), NOW()),
        ('정지아','1990년 부모님의 삶을 소설로 옮긴 장편소설 ''빨치산의 딸''(전 3권)을 발표하며 작품활동을 시작했다.', NOW(), NOW()),
