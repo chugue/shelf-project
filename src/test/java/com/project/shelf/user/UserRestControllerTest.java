d39e006ba8cac156b6d161dede1ce95e35bc2213
LifeIsOne <didtmdgh258@gmail.com>
2024-07-21 19:19:41 +0900

    [FEAT] : 사용자 로그인 테스트

--- src/test/java/com/project/shelf/user/UserRestControllerTest.java
+++ src/test/java/com/project/shelf/user/UserRestControllerTest.java
@@ -3,6 +3,8 @@ package com.project.shelf.user;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.shelf.payment.PortOneService;
+import com.project.shelf.user.UserRequestRecord.LoginReqDTO;
+import com.project.shelf.user.UserResponseRecord.LoginRespDTO;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -70,5 +72,42 @@ public class UserRestControllerTest {
         actions.andExpect(MockMvcResultMatchers.jsonPath("$.data.id").value(11));
         actions.andExpect(MockMvcResultMatchers.jsonPath("$.data.email").value("matthew@gmail.com"));
         actions.andExpect(MockMvcResultMatchers.jsonPath("$.data.nickName").value("matthew"));
+    }
+
+    // 로그인 테스트
+    @Test
+    public void login_test() throws Exception {
+        // given
+        System.out.println("💕💕💕💕💕테스트 실행");
+        String email    = "ysh@naver.com";
+        String password = "1234";
+
+        LoginReqDTO reqDTO = new LoginReqDTO(email, password);
+        System.out.println("요청 DTO : " + reqDTO.toString());
+
+        String reqBody = new ObjectMapper().writeValueAsString(reqDTO);
+        System.out.println("요청 바디 : " + reqBody);
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/user/login")
+                        .content(reqBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // eye
+        String respBody = actions.andReturn().getResponse().getContentAsString();
+        int statusCode = actions.andReturn().getResponse().getStatus();
+        System.out.println("응답 바디 : " + respBody);
+        System.out.println("상태 코드 : " + statusCode);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.status().isOk());
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.data.id").value(6));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.data.email").value("ysh@naver.com"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.data.nickName").value("ysh"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.data.avatar").value("AVATAR06"));
     }
 }
